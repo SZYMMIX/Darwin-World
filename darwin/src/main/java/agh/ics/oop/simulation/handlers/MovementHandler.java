f463f40af0aad981c956baf78adf3f049dddc01a
Marcin <ottemarcin@gmail.com>
2026-01-14 23:21:36 +0100

    feat(simulation): add movement handler skeleton

---
+++ darwin/src/main/java/agh/ics/oop/simulation/handlers/MovementHandler.java
@@ -0,0 +1,11 @@
+package agh.ics.oop.simulation.handlers;
+
+import java.util.List;
+
+import agh.ics.oop.model.Animal;
+
+public class MovementHandler {
+    public void move(List<Animal> animals) {
+        // TODO:
+    }
+}
