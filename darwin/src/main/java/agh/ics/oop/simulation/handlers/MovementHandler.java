f55d47713d246a122048a94da3f9865489ff60de
Marcin <ottemarcin@gmail.com>
2026-01-14 23:51:17 +0100

    refactor(simulation): refactor movement handler to consume simulation fields

--- darwin/src/main/java/agh/ics/oop/simulation/handlers/MovementHandler.java
+++ darwin/src/main/java/agh/ics/oop/simulation/handlers/MovementHandler.java
@@ -3,9 +3,22 @@ package agh.ics.oop.simulation.handlers;
 import java.util.List;
 
 import agh.ics.oop.model.Animal;
+import agh.ics.oop.simulation.SimulationParameters;
 
 public class MovementHandler {
-    public void move(List<Animal> animals) {
+    private final int width;
+    private final int height;
+
+    private final List<Animal> animals;
+
+    public MovementHandler(SimulationParameters parameters, List<Animal> animals) {
+        this.width = parameters.width();
+        this.height = parameters.height();
+
+        this.animals = animals;
+    }
+
+    public void handle() {
         // TODO:
     }
 }
