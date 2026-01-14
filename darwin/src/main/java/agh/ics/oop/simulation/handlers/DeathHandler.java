52ad98122278d2f34b9941991d23f92ce782565b
Marcin <ottemarcin@gmail.com>
2026-01-14 23:42:49 +0100

    refactor(simulation): refactor death handler to consume simulation fields

--- darwin/src/main/java/agh/ics/oop/simulation/handlers/DeathHandler.java
+++ darwin/src/main/java/agh/ics/oop/simulation/handlers/DeathHandler.java
@@ -1,5 +1,6 @@
 package agh.ics.oop.simulation.handlers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import agh.ics.oop.model.Animal;
@@ -8,15 +9,21 @@ import agh.ics.oop.simulation.SimulationParameters;
 public class DeathHandler {
     private final int dailyEnergyCost;
 
-    public DeathHandler(SimulationParameters parameters) {
+    private final ArrayList<Animal> animals;
+    private final ArrayList<Animal> dead;
+
+    public DeathHandler(SimulationParameters parameters, ArrayList<Animal> animals, ArrayList<Animal> dead) {
         this.dailyEnergyCost = parameters.dailyEnergyCost();
+
+        this.animals = animals;
+        this.dead = dead;
     }
 
-    public void handle(List<Animal> alive, List<Animal> dead) {
+    public void handle() {
         // TODO:
     }
 
-    public void energy(List<Animal> animals) {
+    public void subtractEnergy() {
         // TODO:
     }
 }
