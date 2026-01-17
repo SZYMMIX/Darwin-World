5e544fdc07d87ddd02adb076f95f599a10b1544f
Marcin Otte <ottemarcin@gmail.com>
2026-01-17 02:12:48 +0100

    feat(simulation): add random child data generation to create animals only using child data record

--- darwin/src/main/java/agh/ics/oop/simulation/ChildData.java
+++ darwin/src/main/java/agh/ics/oop/simulation/ChildData.java
@@ -3,10 +3,25 @@ package agh.ics.oop.simulation;
 import agh.ics.oop.model.Genotype;
 import agh.ics.oop.model.Vector2d;
 
+import java.util.Random;
+
 record ChildData(
         Vector2d position,
         int initialEnergy,
         Genotype genotype,
-        int parentAId,
-        int parentBId
-) {}
+        Integer parentAId,
+        Integer parentBId
+) {
+    static ChildData random(int width, int height, int initialAnimalEnergy, int genotypeLength, Random random) {
+        Vector2d randomPosition = new Vector2d(random.nextInt(width), random.nextInt(height));
+        Genotype randomGenotype = Genotype.random(genotypeLength, random);
+
+        return new ChildData(
+                randomPosition,
+                initialAnimalEnergy,
+                randomGenotype,
+                null,
+                null
+        );
+    }
+}
